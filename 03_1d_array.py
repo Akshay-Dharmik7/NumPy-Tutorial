10412ad03d57b1bd2445bedf0f50f15e842400ac
Akshay-Dharmik7 <akshaydharmik72@gmail.com>
2026-05-21 09:49:39 +0530

    add types of array

---
+++ 03_1d_array.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+num_arr = np.array([1, 2, 3, 4, 5, 6])
+
+print(num_arr)
+
+char_arr = np.array(['a', 'b', 'c', 'd', 'e', 'f'])
+print(char_arr)
+
+alphanum_arr = np.array([1, 2, 'a', 4, 'b', 5])
+print(alphanum_arr)
