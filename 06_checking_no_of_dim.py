b2c46ddb87d2ecc9920a2fc5c0f5f73a3d798230
Akshay-Dharmik7 <akshaydharmik72@gmail.com>
2026-05-21 10:04:35 +0530

    check no of dimensions of array

--- 06_checking_no_of_dim.py
+++ 06_checking_no_of_dim.py
@@ -1,2 +1,14 @@
+import numpy as np
 
+num_arr_0d = np.array(45)
+print(num_arr_0d.ndim)
+
+num_arr_1d = np.array([1, 2, 3, 4, 5, 6])
+print(num_arr_1d.ndim)
+
+num_arr_2d = np.array([[1, 2, 3], [4, 5, 6]])
+print(num_arr_2d.ndim)
+
+num_arr_3d = np.array([[[1, 2, 3], [4, 5, 6]], [[3, 6, 4], [1, 5, 3]]])
+print(num_arr_3d.ndim)
 
