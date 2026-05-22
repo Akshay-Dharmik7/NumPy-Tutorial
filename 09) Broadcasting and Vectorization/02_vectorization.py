a015f3157c7f740b282a1ae82d82282c25211bc6
Akshay-Dharmik7 <akshaydharmik72@gmail.com>
2026-05-22 10:23:50 +0530

    vectorization in numpy

--- 09) Broadcasting and Vectorization/02_vectorization.py
+++ 09) Broadcasting and Vectorization/02_vectorization.py
@@ -1,2 +1,12 @@
+import numpy as np
+arr = np.array([1, 2, 3])
 
+new_arr = arr * 10
+
+print(new_arr)
+
+# Vectorized Mathematical Functions
+# NumPy applies functions element-wise.
+
+print(np.sqrt(arr))
 
