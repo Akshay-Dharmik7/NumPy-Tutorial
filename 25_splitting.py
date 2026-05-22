510cf84d52989a8cc9b7737a8e7090a945830a2a
Akshay-Dharmik7 <akshaydharmik72@gmail.com>
2026-05-22 08:27:35 +0530

    add more in splitting an array

--- 25_splitting.py
+++ 25_splitting.py
@@ -1,11 +1,14 @@
 import numpy as np
 
 arr1 = np.array([1, 2, 3, 4, 5, 6, 7, 6])
-print(arr1)
+arr2 = np.array([[1, 2], [4, 5]])
+arr3 = np.array([[[1, 2], [4, 5]]])
 
 # split- not allow unequal splitting
 # splitting 1d array
 print(np.split(arr1, 2))
+print(np.split(arr2, 2, axis=0))
+print(np.split(arr2, 2, axis=1))
 
 # splitting array using index position
 print(np.split(arr1, [3]))
@@ -15,10 +18,12 @@ print(np.array_split(arr1, 3))  #allow unequal splitting
 
 
 # vsplit and hsplit work on 2d array
-arr2 = np.array([[1, 2], [4, 5]])
 # vsplit
 print(np.vsplit(arr2, 2))
 
 # hsplit
 print(np.hsplit(arr2, 2))
+
+# dsplit
+print(np.dsplit(arr3, 2))
 
