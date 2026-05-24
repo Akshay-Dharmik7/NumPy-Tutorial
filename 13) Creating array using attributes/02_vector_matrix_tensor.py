c840ffa6e3c2e3959416c5116da2a9839bc1802b
Akshay-Dharmik7 <akshaydharmik72@gmail.com>
2026-05-24 15:55:44 +0530

    vector, matrix, tenson

---
+++ 13) Creating array using attributes/02_vector_matrix_tensor.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+vector = np.array([1, 2, 3, 4, 5, 6])
+print(vector)
+
+matrix = np.array([
+                    [1,  2, 3], 
+                    [4, 5, 6]
+                ])
+print(matrix)
+
+tensor = np.array([
+                    [
+                        [1, 2],
+                        [3, 4]
+                    ], 
+                    [
+                        [5, 6],
+                        [7, 8]
+                    ]
+                ])
+print(tensor)
